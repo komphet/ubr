618f7a54fa84b1ba42eefad239e31b78dcf15db0
komphet <komphetmeesab@hotmail.com>
2016-02-24 11:04:05 +0700

    d

--- config/app.php
+++ config/app.php
@@ -145,7 +145,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+        //Intervention\Image\ImageServiceProvider::class,
 
 
         //Htmldom
@@ -202,7 +202,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+        //'Image' => Intervention\Image\Facades\Image::class,
 
         //Htmldom
         //'Htmldom' => Yangqi\Htmldom\Htmldom::class,
